cf059736a9eb6b7f23d4ad72d57993b96fa86952
Marcelo <ma.darras@duocuc.cl>
2026-07-25 00:51:38 -0400

    Mas configuraciones para railway

---
+++ manage.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# Agregar la carpeta backend al path de Python
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+
+try:
+    from backend.app import app
+except ImportError:
+    from app import app
+
+from waitress import serve
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    print(f"🚀 Iniciando Servidor desde manage.py puente en http://0.0.0.0:{port}")
+    serve(app, host="0.0.0.0", port=port)
